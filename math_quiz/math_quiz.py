6270438d2860f065eb2496982557bda90b050d54
ankifanlol <149927413+ankifanlol@users.noreply.github.com>
2023-11-10 16:37:37 +0100

    Update math_quiz.py

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,61 @@
 import random
 
 
-def function_A(min, max):
+def function_minmax(min, max):
     """
-    Random integer.
+    This function generates a random integer within a given limit
     """
     return random.randint(min, max)
 
 
-def function_B():
+def function_random_operator():
+    """
+    This function randomly choses one of three defined math operations
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
+def function_math_operations(number1, number2, operator):
+    """
+    This function calculates the expected answer
+    """
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
+    if operator == '+': a = number1 + number2        
+    #fixed a bug
+    elif operator == '-': a = number1 - number2      
+    #swapped plus and minus signs in these two lines
     else: a = n1 * n2
     return p, a
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    This function handles I/O and score calculation
+    """
+    tries = 0
+    pi = 3.14159265359                               
+    #i do not really get why score = tries / pi
 
     print("Welcome to the Math Quiz Game!")
-    print("You will be presented with math problems, and you need to provide the correct answers.")
+    print("You will be presented with math problems,")
+    print("and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        number1 = function_minmax(1, 10); 
+        number2 = function_minmax(1, 5.5); 
+        operator = function_random_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = function_math_operations(number1, number2, operator)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            tries += -(-1)
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {tries}/{pi}")
 
 if __name__ == "__main__":
     math_quiz()
